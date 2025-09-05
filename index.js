9a1d0c172851ab0d9c5fa183a58bd444d3789e2b
Era-smus <erasmussaat1@gmail.com>
2025-09-05 18:28:35 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield"] 
+ cats.push();// Write your solution here!
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function  destructivelyRemoveLastCat(name){
+    cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat(name){
+cats.shift("Milo")
+}
+function appendCat(name){
+    return [...cats, name];
+}
+function prependCat(name){
+    return[name, ...cats];
+}
+function removeLastCat(name){
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat(name){
+    return cats.slice(1);
+}
+
+
